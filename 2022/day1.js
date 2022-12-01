733fda527b711c51a1148dab1a3b5a12510652ac
tomrni <90772332+gitoror@users.noreply.github.com>
2022-12-01 07:36:30 +0100

    day1

---
+++ 2022/day1.js
@@ -0,0 +1,26 @@
+f = "day1/input.txt";
+const fs = require("fs");
+const input = fs.readFileSync(`./inputs/${f}`, "utf-8").trim().split("\n\n");
+
+// Part 1
+const maxW = (input) => {
+  weights = input.map((L) => {
+    L = L.split("\n").map(Number);
+    return L.reduce((acc, v) => acc + v);
+  });
+  return weights.sort().at(-1);
+};
+console.log(maxW(input));
+
+// Part 2
+const top3 = (input) => {
+  weights = input.map((W) => {
+    W = W.split("\n").map(Number);
+    return W.reduce((acc, v) => acc + v);
+  });
+  return weights
+    .sort()
+    .slice(-3)
+    .reduce((acc, v) => acc + v);
+};
+console.log(top3(input));
